1ebb491162238515f879c67c7639dd3afebe4493
hayderAbdulsahib <abdulsahib.hayder@gmail.com>
2022-06-01 22:32:19 +0300

    creating the connection with the database

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//start building the project 
+const express = require("express");
+
+//since the require is inside the index.js it will affect all the files of the project
+require("dotenv").config();
+
+//we required the db file in order to make the connection with the database
+require("./models/db");
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
