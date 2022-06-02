e3dfeacc4d9e5c36e46913d45ee14cfe3b7cf2e8
MustafaMHamdan <hamdan.mustafamof@gmail.com>
2022-06-02 11:42:33 +0300

    authorization

---
+++ middleware/authorization.js
@@ -0,0 +1,27 @@
+const connection = require("../models/db");
+
+//this function build to check if the user has the authorati to make some function or not
+
+const authorization = (string) => {
+  return function (req, res, next) {
+    const user_id = req.token.userId;
+    const data = [user_id];
+    const command = `SELECT * FROM users U WHERE U.id = (?)`;
+    connection.query(command, data, (err, result) => {
+      console.log(result);
+      const command2 = `SELECT * FROM role_permission RP INNER JOIN permissions P ON RP.permission_id = P.id WHERE RP.role_id = (?) AND P.permission = (?)`;
+
+      const data = [result[0].role_id, string];
+
+      connection.query(command2, data, (err, result) => {
+        if (result.length) {
+          next();
+        } else {
+          res.status(400).json({ message: "unauthorized" });
+        }
+      });
+    });
+  };
+};
+
+module.exports = authorization;
