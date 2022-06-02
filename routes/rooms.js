84943e48eac726289fc8c446ef3521591a563ed6
MosaSaleh <mosa.abed.saleh@gmail.com>
2022-06-02 23:58:54 +0300

    git all my rooms

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,28 +1,29 @@
 const express = require("express");
 
-const authentication = require('../middleware/authentication')
-const authorization = require('../middleware/authorization')
+const authentication = require("../middleware/authentication");
+const authorization = require("../middleware/authorization");
 //room routes
 
 const {
   getAllGroupRooms,
   getRoomById,
+  updateRoomById,
+  getAllMyRooms,
   deleteRoomById,
 } = require("../controllers/rooms");
 
- const {getAllGroupRooms,getRoomById,updateRoomById}=require("../controllers/rooms")
+const roomRouter = express.Router();
 
-const roomRouter =express.Router();
+roomRouter.get("/group", getAllGroupRooms);
+roomRouter.get("/my_rooms", authentication, getAllMyRooms);
 
-
-roomRouter.get("/group",getAllGroupRooms)
-roomRouter.get("/:id",getRoomById)
-roomRouter.put("/:id",updateRoomById)
-roomRouter.delete("/:id",authentication ,authorization(`DELETE_ROOMS`),  deleteRoomById);
-
-
-
- 
- 
+roomRouter.get("/:id", getRoomById);
+roomRouter.put("/:id", updateRoomById);
+roomRouter.delete(
+  "/:id",
+  authentication,
+  authorization(`DELETE_ROOMS`),
+  deleteRoomById
+);
 
 module.exports = roomRouter;
