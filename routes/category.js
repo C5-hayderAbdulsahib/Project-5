0071f551663af30df879111c97369a8e1809fce9
MustafaMHamdan <hamdan.mustafamof@gmail.com>
2022-06-02 13:47:51 +0300

    add auth

--- routes/category.js
+++ routes/category.js
@@ -2,14 +2,18 @@
 
 const express  = require("express")
 
+const authentication =require("../middleware/authentication")
+const authorization =require("../middleware/authorization")
+
+
 
 const { createNewRoomCategory ,getAllCategories ,updateCategoryById} = require("../controllers/category")
 
 const categoryRouter = express.Router();
 
-categoryRouter.post("/", createNewRoomCategory);
-categoryRouter.put("/:id", updateCategoryById);
-categoryRouter.get("/" , getAllCategories)
+categoryRouter.post("/", authentication,authorization("CREATE_CATEGORIES"),createNewRoomCategory);
+categoryRouter.put("/:id",authentication, authorization("Update_Categories"), updateCategoryById);
+categoryRouter.get("/" , authentication,getAllCategories)
 
 module.exports = categoryRouter;
 
