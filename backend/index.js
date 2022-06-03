fc1257393a2acdd1fb5d0f730868ce03635aefd3
hayderAbdulsahib <abdulsahib.hayder@gmail.com>
2022-06-03 19:26:43 +0300

    adding cores to checkout page

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+//adding the cores package
+const cors = require("cors");
 
 //since the require is inside the index.js it will affect all the files of the project
 require("dotenv").config();
@@ -7,6 +9,7 @@ require("dotenv").config();
 require("./models/db");
 
 const app = express();
+app.use(cors()); //we created an application-level middleware and invoke it the cors function, and the reason for that if we want these api that we created to be used by the frontend code then we have to give it the cors permissions
 
 //here we will import the routers
 
