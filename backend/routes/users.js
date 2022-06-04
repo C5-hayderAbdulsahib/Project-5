36dd8dbef4c9d429ee792f8a29fba21c99d0d6b9
hayderAbdulsahib <abdulsahib.hayder@gmail.com>
2022-06-05 00:08:58 +0300

    automatic-signup-backedn

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,14 +1,19 @@
 const express = require("express");
 
-const { signup, signIn, getAllUsernames ,getUserInfo} = require("../controllers/users");
+const {
+  signup,
+  signIn,
+  getAllUsernames,
+  getUserInfo,
+} = require("../controllers/users");
 
 ////////////Middleware////////
 const authentication = require("../middleware/authentication");
 
 //===================================================
 const usersRouter = express.Router();
- usersRouter.get("/",authentication, getUserInfo); 
-usersRouter.post("/signup", signup);
+usersRouter.get("/", authentication, getUserInfo);
+usersRouter.post("/signup", signup, signIn);
 usersRouter.post("/signIn", signIn);
 usersRouter.get("/usernames", authentication, getAllUsernames);
 
