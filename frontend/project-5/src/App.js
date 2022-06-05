0f863caed3445d02794c222b56d78222731a9089
MustafaMHamdan <hamdan.mustafamof@gmail.com>
2022-06-05 16:04:00 +0300

    updateroom

--- frontend/project-5/src/App.js
+++ frontend/project-5/src/App.js
@@ -1,6 +1,6 @@
 //importing packages
 import { Routes, Route } from "react-router-dom";
-
+import Mustafa from "./user components/Mustafa";
 //import components
 import SuperSigninPage from "./superadmin components/SuperSigninPage"; //since the file inside the Register has the name of index.js then there is no need to specify it in the import path like this "./components/Register/index" because if you specify a folder then it look inside it files and if it saw a file with the name index then it will import that one, but if we want to import a file with another name then we need so specify it in the import path or it will not be imported
 
@@ -31,6 +31,7 @@ function App() {
         <LeftMyRooms />
 
         <Routes>
+        <Route path="/Mustafa" element={<Mustafa />} />
           <Route path="/update" element={<UpdateRoomModal />} />
           <Route path="/delete" element={<DeleteRoomModal />} />
           <Route path="/create" element={<CreateRoomModal />} />
