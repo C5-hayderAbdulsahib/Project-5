c473cbb1da18fd2c4e12f5531e2dfb76e7d02d5e
MosaSaleh <mosa.abed.saleh@gmail.com>
2022-06-05 19:11:57 +0300

    get-all-rooms

--- frontend/project-5/src/user components/Mosa/GetAllMyRooms.js
+++ frontend/project-5/src/user components/Mosa/GetAllMyRooms.js
@@ -1,9 +1,42 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { setRooms } from "../../redux/reducers/rooms";
+
 export const GetAllMyRooms = () => {
+  const dispatch = useDispatch();
+  const { token, rooms } = useSelector((state) => {
+    return { token: state.auth.token, rooms: state.rooms.rooms };
+  });
+  const getAllRooms = () => {
+    axios
+      .get(`http://localhost:5000/rooms/my_rooms`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((result) => {
+        dispatch(setRooms(result.data.rooms));
+      });
+  };
+
+  useEffect(() => {
+    getAllRooms();
+  }, []);
+
   return (
     <>
       <div>
         <div>
           <p>get my rooms</p>
+          {rooms &&
+            rooms.map((element) => {
+              return (
+                <div key={element.id}>
+                  <p>{element.name}</p>
+                </div>
+              );
+            })}
         </div>
       </div>
     </>
