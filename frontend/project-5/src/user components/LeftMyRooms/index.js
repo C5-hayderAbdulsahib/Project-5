6fcef451dbee00c0e45d22d5dace54c59880b2d1
hayderAbdulsahib <abdulsahib.hayder@gmail.com>
2022-06-06 13:31:34 +0300

    getallmyrooms frontend page created

--- frontend/project-5/src/user components/LeftMyRooms/index.js
+++ frontend/project-5/src/user components/LeftMyRooms/index.js
@@ -1,7 +1,7 @@
 //import packages
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { getAllMyRooms } from "../../redux/reducers/rooms";
 
@@ -17,30 +17,44 @@ const LeftMyRooms = () => {
     return { token: state.auth.token, rooms: state.rooms.rooms };
   });
 
+  const [message, setMessage] = useState("");
+
+  //============================================================
+  const getAllRooms = () => {
+    axios
+      .get(`http://localhost:5000/rooms/my_rooms`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((result) => {
+        dispatch(getAllMyRooms(result.data.rooms));
+        if (result.data.message === "No Room Were Created Yet")
+          setMessage(result.data.message);
+      });
+  };
+  //============================================================
+  useEffect(() => {
+    getAllRooms();
+  }, []);
+
   return (
     <>
       <div className="display-grid">
         <div>
-          <p>this is the left side with all my rooms</p>
-          <p>this is the left side with all my rooms</p>
-          <p>this is the left side with all my rooms</p>
-          <p>this is the left side with all my rooms</p>
-          <p>this is the left side with all my rooms</p>
-          <p>this is the left side with all my rooms</p>
-          <p>this is the left side with all my rooms</p>
-          <p>this is the left side with all my rooms</p>
-          <p>this is the left side with all my rooms</p>
-          <p>this is the left side with all my rooms</p>
-          <p>this is the left side with all my rooms</p>
-          <p>this is the left side with all my rooms</p>
-          <p>this is the left side with all my rooms</p>
-          <p>this is the left side with all my rooms</p>
-          <p>this is the left side with all my rooms</p>
-          <p>this is the left side with all my rooms</p>
-          <p>this is the left side with all my rooms</p>
-          <p>this is the left side with all my rooms</p>
-          <p>this is the left side with all my rooms</p>
-          <p>this is the left side with all my rooms</p>
+          <div>
+            {rooms &&
+              rooms.map((element) => {
+                return (
+                  <div key={element.id}>
+                    <Link to={`/rooms/${element.id}`}>
+                      <p>{element.name}</p>
+                    </Link>
+                  </div>
+                );
+              })}
+          </div>
+          {message && <p>{message}</p>}
         </div>
         <div>
           <Routes>
