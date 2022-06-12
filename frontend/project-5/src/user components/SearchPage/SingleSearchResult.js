9b701af8c95fafcac3b9bed43b676c5f007b9234
hayderAbdulsahib <abdulsahib.hayder@gmail.com>
2022-06-12 09:33:19 +0300

    changes-serchpage-3

--- frontend/project-5/src/user components/SearchPage/SingleSearchResult.js
+++ frontend/project-5/src/user components/SearchPage/SingleSearchResult.js
@@ -6,11 +6,17 @@ import axios from "axios";
 const SingleSearchResult = (props) => {
   const { search, renderPage, setRenderPage } = props; //we used destructuring to make it easier to use them
 
-  const { rooms, token } = useSelector((state) => {
-    return { rooms: state.rooms.rooms, token: state.auth.token };
+  const { rooms, token, user } = useSelector((state) => {
+    return {
+      rooms: state.rooms.rooms,
+      token: state.auth.token,
+      user: state.user.user,
+    };
   });
 
-  console.log("the room id is", search);
+  console.log("this is me", user.id);
+
+  // console.log("the room id is", search);
 
   const dispatch = useDispatch();
 
@@ -37,25 +43,46 @@ const SingleSearchResult = (props) => {
       });
   };
 
-  // const FollowRoom = () => {
-  //   axios
-  //     .post(
-  //       `http://localhost:5000/rooms/individual_room`,
-  //       { userId },
-  //       {
-  //         headers: {
-  //           Authorization: `Bearer ${token}`,
-  //         },
-  //       }
-  //     )
-  //     .then((result) => {
-  //       setRenderPage(!renderPage);
-  //       console.log(result);
-  //     })
-  //     .catch((err) => {
-  //       throw err;
-  //     });
-  // };
+  const FollowRoom = (roomId) => {
+    axios
+      .post(
+        `http://localhost:5000/rooms/${roomId}/follow`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((result) => {
+        console.log(result);
+        setRenderPage(!renderPage);
+      })
+      .catch((err) => {
+        throw err;
+      });
+  };
+
+  const unFollowRoom = (roomId) => {
+    console.log("the id is", roomId);
+    axios
+      .put(
+        `http://localhost:5000/rooms/${roomId}/unfollow`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((result) => {
+        console.log(result);
+        setRenderPage(!renderPage);
+      })
+      .catch((err) => {
+        throw err;
+      });
+  };
 
   let inRoom = false;
   let alreadyFollowed = false;
@@ -101,7 +128,13 @@ const SingleSearchResult = (props) => {
           <p>this is a group room</p>
 
           {rooms.forEach((element) => {
-            if (element.is_group === 1 && element.room_id === search.id) {
+            console.log(element);
+            if (
+              element.is_group === 1 &&
+              element.room_id === search.id &&
+              element.user_id &&
+              element.is_member === 1
+            ) {
               inRoom = true;
             }
             if (element.send_follow_request === 1) {
@@ -114,9 +147,11 @@ const SingleSearchResult = (props) => {
               <button>view Room</button>
             </Link>
           ) : alreadyFollowed ? (
-            <button>unfoolow room</button>
+            <button onClick={() => unFollowRoom(search.id)}>
+              unfoolow room
+            </button>
           ) : (
-            <button>foolow room</button>
+            <button onClick={() => FollowRoom(search.id)}>foolow room</button>
           )}
 
           <hr></hr>
