f17331762ff4d7f49b06092cba547ebb604ac8ce
hayderAbdulsahib <abdulsahib.hayder@gmail.com>
2022-06-17 16:34:57 +0300

    more-changes

--- frontend/project-5/src/user components/Mustafa/DeleteRoomModal.js
+++ frontend/project-5/src/user components/Mustafa/DeleteRoomModal.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
- 
+
 //import actions
 
 import { deleteRoom } from "../../redux/reducers/rooms";
-
 
 //import styling
 import "./style.css";
@@ -15,16 +14,12 @@ import { RiCloseLine } from "react-icons/ri";
 
 export const DeleteRoomModal = (props) => {
   const {
-     
-
     // logout,
     setIsOpenDelete,
     // token,
   } = props;
 
- 
   const dispatch = useDispatch("");
-
 
   //======================================================================================================
 
@@ -32,12 +27,11 @@ export const DeleteRoomModal = (props) => {
     return { token: state.auth.token, rooms: state.rooms.rooms };
   });
 
-
   const deleteRoomFun = (id = 1) => {
     axios
       .delete(
         `http://localhost:5000/rooms/${id}`,
-      
+
         {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -45,53 +39,17 @@ export const DeleteRoomModal = (props) => {
         }
       )
       .then((result) => {
-        dispatch(deleteRoom( id));
+        dispatch(deleteRoom(id));
       })
       .catch((err) => {
         console.log(err);
       });
   };
 
-
   return (
     <>
       {/* the onclick event that we add so that if the user click outside the model anywhere in the window it will close the model and we can remove this part if we want to */}
-      <div className="darkBG" onClick={() => setIsOpenDelete(false)} />
-      <div className="centered">
-        <div className="modal">
-          <div className="modalHeader">
-            <h5 className="heading">Dialog</h5>
-          </div>
-          <button className="closeBtn" onClick={() => setIsOpenDelete(false)}>
-            <RiCloseLine style={{ marginBottom: "-3px" }} />
-          </button>
-
-          <div className="modalContent">
-            {/* ///////////////////////////////the body f the model */}
-            Are you sure you want to delete the name of this Room?
-            <div className="push-down"></div>
-          
-               
-        
-              <div className="push-down"></div>
-              {/* the update button */}
-              <button
-                 onClick={() => {
-                  deleteRoomFun();
-                  setIsOpenDelete(false);
-                 }}
-                className="deleteBtn"
-              >
-                Delete Room
-              </button>
-              {/* the cancel model button */}
-              <button className="cancelBtn" onClick={() => setIsOpenDelete(false)}>
-                Cancel
-              </button>
-           
-          </div>
-        </div>
-      </div>
+      <div></div>
     </>
   );
 };
